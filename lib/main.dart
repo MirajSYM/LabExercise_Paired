a127fad719da1fb009b0a59527f9e78821d1b27d
MirajSYM <mirajahmedsymanto@gmail.com>
2020-04-09 22:17:35 +0800

    Adding functionality to the buttons

--- lib/main.dart
+++ lib/main.dart
@@ -24,31 +24,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double _size = 150.0;
+  get size => _size;
+  set size(value) => setState(() {
+        _size = value;
+      });
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlutterLogo(size: 150.0),
+        child: FlutterLogo(size: _size),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FloatingActionButton.extended(
-            onPressed: () {},
-            label: Text('Small'),
-            icon: Icon(Icons.photo_size_select_small),
-          ),
-          FloatingActionButton.extended(
-            onPressed: () {},
-            label: Text('Medium'),
-            icon: Icon(Icons.photo_size_select_actual),
-          ),
-          FloatingActionButton.extended(
-            onPressed: () {},
-            label: Text('Large'),
-            icon: Icon(Icons.photo_size_select_large),
-          ),
-        ],
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(
+            left:
+                22.0), //Padding is set to left to align the children in center
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            button(
+              'Small',
+              Icon(Icons.photo_size_select_small),
+              () {
+                size = 50.0;
+              },
+            ),
+            button(
+              'Medium',
+              Icon(Icons.image),
+              () {
+                size = 150.0;
+              },
+            ),
+            button(
+              'Large',
+              Icon(Icons.photo_size_select_large),
+              () {
+                size = 300.0;
+              },
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: SizedBox(
         height: 150.0,
@@ -85,5 +101,13 @@ Slider slider(value, function, max) {
     max: max,
     value: value,
     onChanged: function,
+  );
+}
+
+FloatingActionButton button(btnName, icon, function) {
+  return FloatingActionButton.extended(
+    onPressed: function,
+    label: Text(btnName),
+    icon: icon,
   );
 }
